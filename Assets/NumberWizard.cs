1849c812f96b13a082dc7ac4d5d3b1f8f293f755
Roberto Cano <gabr1e11@hotmail.com>
2017-01-06 18:13:16 +0100

    Added liar detector

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,41 +7,64 @@ using UnityEngine.SceneManagement;
 public class NumberWizard : MonoBehaviour {
 
 	public Text guessText;
+	public Text liarText;
 	public int min;
 	public int max;
 	public int leftGuesses;
 
 	int guess;
+	int liarCount;
+	int turn;
 
 	// Use this for initialization
 	void Start () {
 		min = 0;
 		max = 1000;
 		guess = (max - min) / 2;
-		leftGuesses = 5;
+		leftGuesses = 10;
+		liarCount = 0;
+		liarText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (leftGuesses == 0) {
+		if (leftGuesses < 0) {
 			SceneManager.LoadScene ("Lose");
+			return;
 		}
+
+		if (liarCount > 0) {
+			liarText.text = "";
+			for (int i = 0; i < liarCount; ++i)
+				liarText.text += "LIAR!! ";
+		}
+
 		guessText.text = "I think your number is: " + guess + "\n";
 		guessText.text += "Tries left: " + leftGuesses + "\n";
 	}
 
 	private void CalculateNextGuess() {
 		leftGuesses--;
-		guess = (min + max) / 2;
+		guess = Random.Range(min, max);
 	}
 
 	public void OnHigherClick()	{
-		min = guess;
+		if (min == max) {
+			liarCount++;
+			return;
+		}
+		liarCount = 0;
+		min = guess + 1;
 		CalculateNextGuess ();
 	}
 
 	public void OnLowerClick() {
-		max = guess;
+		if (min == max) {
+			liarCount++;
+			return;
+		}
+		liarCount = 0;
+		max = guess - 1;
 		CalculateNextGuess ();
 	}
 
